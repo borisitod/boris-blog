165ccd57995b0c9a2df0ceb35e7e66508574d6f0
borisitod <boris@itondemand.com.au>
2017-01-04 19:59:12 +1100

    implement delete function for post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -123,5 +123,13 @@ class PostController extends Controller
     public function destroy($id)
     {
         //
+        $post = Post::find($id);
+
+        $post->delete();
+
+        Session::flash('success','This post was successfully deleted.');
+
+        return redirect()->route('posts.index');
+
     }
 }
