517cf85e4063df725e854207b3d1b19b7175c74f
borisitod <boris@itondemand.com.au>
2017-01-08 21:53:54 +1100

    implement relationship between post and category

--- app/Post.php
+++ app/Post.php
@@ -11,4 +11,8 @@ class Post extends Model
     protected $fillable = [
         'title', 'body'
     ];
+
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
 }
